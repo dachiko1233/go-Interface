397c617c1a92029d0f967687801d37b9ef82f2db
dachiko1233 <dachimari9@email.com>
2026-02-23 18:53:48 +0200

    go-Interface and testing

---
+++ main_test.go
@@ -0,0 +1,50 @@
+package main
+
+import "testing"
+
+// func TestPermeter(t *testing.T) {
+// 	rectangle := Rectangle{10.0, 10.0}
+// 	got := Premeter(rectangle)
+// 	want := 40.0
+
+// 	if got != want {
+// 		t.Errorf("got %.2f want %.2f", got, want)
+// 	}
+// }
+
+func TestArea(t *testing.T) {
+
+	// checkArea := func(t testing.TB, shape Shape, want float64) {
+	// 	t.Helper()
+	// 	got := Shape.Area(shape)
+	// 	if got != want {
+	// 		t.Errorf("got %g want %g", got, want)
+	// 	}
+	// }
+	// t.Run("RecteAgles", func(t *testing.T) {
+	// 	rectangle := Rectangle{12, 6}
+	// 	checkArea(t, rectangle, 72.0)
+	// })
+
+	// t.Run("circles", func(t *testing.T) {
+	// 	circle := Circle{10}
+	// 	checkArea(t, circle, 314.1592653589793)
+	// })
+
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+
+}
